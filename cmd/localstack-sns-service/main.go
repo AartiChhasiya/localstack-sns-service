4c21859c6edd98b1c1ffa0305220e8c59e13c72e
Aarti Parmar <aarti.parmar@contis.com>
2023-02-21 14:12:55 +0530

    publish message

--- cmd/localstack-sns-service/main.go
+++ cmd/localstack-sns-service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -34,15 +35,79 @@ func main() {
 		log.Fatalf("failed to create SNS topic: %v", err)
 	}
 
+	// fmt.Printf("Type of Topic ARN is: %T\n", topic.TopicArn)
+	fmt.Printf("Topic ARN is: %s\n", *topic.TopicArn)
+
 	// Publish a message to the SNS topic
-	message := "Hello, world!"
+	publishMessage(snsClient, *topic.TopicArn)
+
+	// Publish a new message every 5 seconds continuously
+	publishMessageContinuouslyOnInterval(snsClient, *topic.TopicArn, 5)
+
+	// err = godotenv.Load(".env-local")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env-local file")
+	// }
+	// envValue := os.Getenv("PUBLISH_MSG_CONTINUOUSLY")
+
+	// if envValue == "" {
+	// 	fmt.Println("Environment variable is empty")
+	// 	return
+	// }
+
+	// // Convert the environment variable value to a boolean
+	// postMsgContinuously, err := strconv.ParseBool(envValue)
+	// if err != nil {
+	// 	fmt.Println("Failed to parse environment variable:", err)
+	// 	return
+	// }
+
+	// if postMsgContinuously {
+	// 	// Publish a message to the SNS topic
+	// 	publishMessage(snsClient, *topic.TopicArn)
+	// } else {
+
+	// 	interval, err := time.ParseDuration(os.Getenv("PUBLISH_MSG_INTERVAL"))
+	// 	if err != nil {
+	// 		fmt.Println("Failed to parse environment variable:", err)
+	// 		return
+	// 	}
+
+	// 	// Publish a new message every 5 seconds continuously
+	// 	publishMessageContinuouslyOnInterval(snsClient, *topic.TopicArn, interval)
+	// }
+}
+
+func publishMessage(snsClient *sns.SNS, topicARN string) {
+	message := "Hello, from Aarti Chhasiya!"
 	result, err := snsClient.Publish(&sns.PublishInput{
 		Message:  aws.String(message),
-		TopicArn: topic.TopicArn,
+		TopicArn: aws.String(topicARN),
 	})
 	if err != nil {
 		log.Fatalf("failed to publish message: %v", err)
 	}
 
 	fmt.Printf("Published message with ID %s\n", *result.MessageId)
+}
+
+func publishMessageContinuouslyOnInterval(snsClient *sns.SNS, topicARN string, interval time.Duration) {
+	for {
+		// Generate a new message
+		message := fmt.Sprintf("Hello, from Aarti Chhasiya! ==> Message published at %s", time.Now().Format(time.RFC3339))
+
+		// Publish the message to the SNS topic
+		_, err := snsClient.Publish(&sns.PublishInput{
+			Message:  aws.String(message),
+			TopicArn: aws.String(topicARN),
+		})
+		if err != nil {
+			fmt.Println("Failed to publish message:", err)
+		} else {
+			fmt.Println("Message published:", message)
+		}
+
+		// Wait for 5 seconds before publishing the next message
+		time.Sleep(interval * time.Second)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/AartiChhasiya/localstack-sns-service
 
 go 1.19
 
-require github.com/aws/aws-sdk-go v1.44.204
+require (
+	github.com/aws/aws-sdk-go v1.44.204
+	github.com/joho/godotenv v1.5.1
+)
 
 require github.com/jmespath/go-jmespath v0.4.0 // indirect
